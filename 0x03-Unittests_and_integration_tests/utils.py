05602231ad482af683fc8c7a37628a2241f79bbb
grace <xperttechsolutions111@gmail.com>
2025-11-12 22:04:27 +0100

    Fix KeyError handling in access_nested_map and add parameterized tests

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """Utility functions module."""
 
+
 def access_nested_map(nested_map, path):
     """Access nested map using path."""
+    current = nested_map
     for key in path:
-        nested_map = nested_map[key]
-    return nested_map
+        if not isinstance(current, dict) or key not in current:
+            raise KeyError(key)
+        current = current[key]
+    return current
